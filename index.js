9dde907d5fee7418974b5505d2f6947525b28dfe
Samantha brake <sammy.brake@gmail.com>
2019-05-17 04:06:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-
+ let newDrivers = [];
   drivers.map(function(driver) {
-     return driver.toLowerCase();
+     newDrivers.push(driver.toLowerCase());
   });
   return drivers
 };
