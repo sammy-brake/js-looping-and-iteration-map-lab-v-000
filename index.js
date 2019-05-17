c8f90e08bb5310fa91d9c14c271b2d6d18c8fa90
Samantha brake <sammy.brake@gmail.com>
2019-05-17 04:51:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+ let newDrivers = [];
+  drivers.map(function(driver) {
+     newDrivers.push(driver.toLowerCase());
+  });
+  return newDrivers
+};
+
+function nameToAttributes(drivers) {
+  let nameDictionary = [];
+  drivers.map(function(driver){
+        nameDictionary.push({firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]});
+  });
+  return nameDictionary;
+};
+
+
+function attributesToPhrase(drivers) {
+  let newReturn = []
+  drivers.map(function(driver){
+    newReturn.push(driver['name'] + " is from " + driver['hometown']);
+  });
+  return newReturn;
+};
