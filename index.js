7082f11a7765792643ff2daba6c52796ba032d25
Samantha brake <sammy.brake@gmail.com>
2019-05-17 03:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
 
-  drivers.map(function(driver){
+  drivers.map(function(driver) {
     return driver.toLowerCase();
   });
 };
